3aa6ff0dc4cbe62a5ebe272e87fef28fc11bd657
quxian <qu.xian@husky.neu.edu>
2021-09-12 14:47:08 -0700

    delete unused xah keys for bracket and add counsel-etags

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  '(org-babel-load-languages (quote ((awk . t) (emacs-lisp . t) (shell . t))))
  '(package-selected-packages
    (quote
-    (helm multiple-cursors helm-c-yasnippet yasnippet-classic-snippets yasnippet-snippets yasnippet which-key projectile helm-swoop use-package keyfreq graphql-mode rjsx-mode thrift scala-mode yaml-mode treemacs magit))))
+    (counsel-etags helm multiple-cursors helm-c-yasnippet yasnippet-classic-snippets yasnippet-snippets yasnippet which-key projectile helm-swoop use-package keyfreq graphql-mode rjsx-mode thrift scala-mode yaml-mode treemacs magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -64,6 +64,9 @@
 
 ;; projectile
 (require 'init-projectile)
+
+;; tags
+(require 'init-tags)
 
 ;; yasnippet
 (require 'init-yasnippet)
---
+++ lisp/init-tags.el
@@ -0,0 +1,12 @@
+(use-package counsel-etags
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook
+        (lambda ()
+          (add-hook 'after-save-hook
+            'counsel-etags-virtual-update-tags 'append 'local)))
+  :config
+  (setq counsel-etags-update-interval 60)
+  (push "build" counsel-etags-ignore-directories))
+
+(provide 'init-tags)
