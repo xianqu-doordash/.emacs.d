0a6e1dab5191f8d55899eed3ef10fddfca294917
quxian <qu.xian@husky.neu.edu>
2021-09-17 13:42:42 -0700

    change projectile with counsel-projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,11 +1,8 @@
-(use-package projectile
+(use-package counsel-projectile
   :ensure t
-  :init
-  (projectile-mode +1)
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
   :config
   (setq projectile-track-known-projects-automatically nil)
-)
+  :hook (after-init . counsel-projectile-mode)
+  :bind-keymap ("C-c p" . projectile-command-map))
 
 (provide 'init-projectile)
--- lisp/init-window.el
+++ lisp/init-window.el
@@ -5,6 +5,10 @@
   :ensure t
   :custom
   (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (aw-dispatch-always t)
+  (aw-dispatch-alist
+   '((?2 aw-split-window-vert)
+     (?3 aw-split-window-horz)))
   :bind (("s-o" . ace-window)))
 
 (provide 'init-window)
