33adff0ce1a373f074971ffb31fabac4f955c379
Xian Qu <chris.quxian@gmail.com>
2021-09-18 14:50:40 -0700

    simplify ace-window configuration

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -3,12 +3,6 @@
 
 (use-package ace-window
   :ensure t
-  :custom
-  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-  (aw-dispatch-always t)
-  (aw-dispatch-alist
-   '((?2 aw-split-window-vert)
-     (?3 aw-split-window-horz)))
   :bind (("s-o" . ace-window)))
 
 (provide 'init-window)
