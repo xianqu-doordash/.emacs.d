b72058b88816174ef3615cc7343faf50c07d7547
Xian Qu <chris.quxian@gmail.com>
2021-09-18 14:43:11 -0700

    use swiper-isearch instead of isearch

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -13,4 +13,8 @@
   :hook (after-init . counsel-projectile-mode)
   :bind-keymap ("C-c p" . projectile-command-map))
 
+(use-package swiper
+  :ensure t
+  :bind ("C-s" . swiper-isearch))
+
 (provide 'init-ivy)
