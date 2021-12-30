6ca549acc8a75e0684f01ae08bc2454c969ecf98
Xian Qu <quxq@amazon.com>
2021-12-30 15:25:44 -0800

    small refactor of yasnippet

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,23 +1,18 @@
 (use-package yasnippet
-  :ensure t)
-
-(use-package helm-c-yasnippet
-  :ensure t)
-
-(require 'yasnippet)
-
-;; helm yasnippet
-(require 'helm-c-yasnippet)
-
-;; this one just gives the advantage when there are too many snippets that you can not remeber
-;; complements this together with "TAB" provided by IDO probably faster
-(global-set-key (kbd "C-c y") 'helm-yas-complete)
-
-(yas-global-mode 1) ;; or M-x yas-reload-all if you've started YASnippet already.
+  :ensure t
+  :diminish yas-minor-mode
+  :init (yas-global-mode)
+  :config
+  (progn
+    (yas-global-mode)))
 
 ;; note that the i currently use the yasnippet official snippet installed from melpa
 ;; personal snippets are under snippets dir anyway, no need to have separate setup for it
+(use-package yasnippet-snippets
+  :ensure t)
 
-;; TODO: is it possible to know if a key word is completable based on cursor color? see some implementation, but seems not working
+(use-package helm-c-yasnippet
+  :ensure t
+  :bind(("C-c y" . 'helm-yas-complete)))
 
 (provide 'init-yasnippet)
