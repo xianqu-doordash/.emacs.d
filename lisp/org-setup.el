da6678eb54152d6e39b97be2a21031e4f1784b0c
Xian Qu <quxq@amazon.com>
2022-09-04 16:18:55 -0700

    organize org mode.

--- lisp/org-setup.el
+++ lisp/org-setup.el
@@ -1,20 +1,21 @@
-;;plantum uml
-(setq org-plantuml-jar-path (expand-file-name "/Users/quxq/.emacs.d/jar/plantuml.jar"))
-(setq plantuml-default-exec-mode 'jar)
-
-;; python setup
-(setq org-babel-python-command "python3")
-
-;; ob-http
-(maybe-require-package 'ob-http)
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((awk . t)
-   (emacs-lisp . t)
-   (shell . t)
-   (plantuml . t)
-   (http . t)
-   (python .t)))
+(use-package org
+  :mode (("\\.org$" . org-mode))
+  :ensure t
+  :custom
+  (org-plantuml-jar-path (expand-file-name "/Users/quxq/.emacs.d/jar/plantuml.jar"))
+  :config
+  ;;plantum uml
+  (setq plantuml-default-exec-mode 'jar)
+  ;; python setup
+  (setq org-babel-python-command "python3")
+  ;; languages supports
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((awk . t)
+     (emacs-lisp . t)
+     (shell . t)
+     (plantuml . t)
+     (http . t)
+     (python .t))))
 
 (provide 'org-setup)
