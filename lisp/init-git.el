da1da654cff018925b90c8570b7c16de290fe347
Xian Qu <quxq@amazon.com>
2023-02-26 17:06:00 -0800

    add magit stage all and commit

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -3,4 +3,11 @@
   :config
   (setq ediff-window-setup-function 'ediff-setup-windows-plain))
 
+(defun xq-magit-stage-all-and-commit (message)
+  (interactive (list (progn (magit-diff-unstaged) (read-string "Commit Message: "))))
+  (magit-stage-modified)
+  (magit-commit-create (list "-m" message)))
+
+;TODO: is it possible to add this as a new option in the ui?
+
 (provide 'init-git)
