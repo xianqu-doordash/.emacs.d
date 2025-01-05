a899e869678b37ec5429c942b1ccae1b86bf183d
xianqu-doordash <xian.qu@doordash.com>
2025-01-05 15:44:20 -0800

    combines magit and projectile and some notes regarding nvm versionin.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,13 +1,3 @@
-(use-package magit
-  :ensure t
-  :config
-  (setq ediff-window-setup-function 'ediff-setup-windows-plain))
 
-(defun xq-magit-stage-all-and-commit (message)
-  (interactive (list (progn (magit-diff-unstaged) (read-string "Commit Message: "))))
-  (magit-stage-modified)
-  (magit-commit-create (list "-m" message)))
-
-;TODO: is it possible to add this as a new option in the ui?
 
 (provide 'init-git)
--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -10,15 +10,29 @@
   :bind-keymap
   ("C-c p" . projectile-command-map))
 
-(defun xq/maybe-run-nvm-use()
-  (interactive)
-  (when (projectile-verify-file ".nvmrc")
-    (projectile-run-shell-command-in-root "/Users/xian.qu/.nvm/nvm-exec")))
-
 (use-package helm-projectile
   :after (helm projectile)
   :ensure t)
 
 (helm-projectile-on)
+
+;TODO: this is not found to be usable for magit as yet
+;; What is really working is the zshell setup from https://stackoverflow.com/questions/23556330/run-nvm-use-automatically-every-time-theres-a-nvmrc-file-on-the-directory
+;; (defun xq/maybe-run-nvm-use()
+;;   (interactive)
+;;   (when (projectile-verify-file ".nvmrc")
+;;     (projectile-run-shell-command-in-root "/Users/xian.qu/.nvm/nvm-exec")))
+
+(use-package magit
+  :ensure t
+  :config
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain))
+
+(defun xq-magit-stage-all-and-commit (message)
+  (interactive (list (progn (magit-diff-unstaged) (read-string "Commit Message: "))))
+  (magit-stage-modified)
+  (magit-commit-create (list "-m" message)))
+
+;TODO: is it possible to add this as a new option in the ui?
 
 (provide 'init-projectile)
